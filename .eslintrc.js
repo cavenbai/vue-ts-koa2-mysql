75ebc09c9c8c195f4accfc0abeab7458b6fe85a9
baipengfei02 <baipengfei02@megvii.com>
2019-08-21 15:58:00 +0800

    去掉业务代码和暂时关闭tslint检测

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   'extends': [
     'plugin:vue/essential',
     '@vue/standard',
-    '@vue/typescript'
+    // '@vue/typescript'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
